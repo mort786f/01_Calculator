717c68cb87a0d0c789d674f1a9db1ec7326a4c3b
morte <morte@DESKTOP-AJQ2HRF>
2016-09-06 10:28:27 +0200

    Add project files.

---
+++ 01_Calculator/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_Calculator
+{
+    internal class Calculator
+    {
+        internal static int Add(int v1, int v2)
+        {
+            return "v1 + v2";
+        }
+    }
+}
